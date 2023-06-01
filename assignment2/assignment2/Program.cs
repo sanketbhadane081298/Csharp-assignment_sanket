6360d3681b58fc0fbe82fb8f117dcbcec6d881e7
sanketbhadane081298 <113082991+sanketbhadane081298@users.noreply.github.com>
2023-06-01 10:37:54 +0530

    Add files via upload

---
+++ assignment2/assignment2/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+public abstract class Clothing
+{
+    public abstract void Sales();
+
+    public abstract void Materials();
+
+    public abstract void CommonWear();
+
+    public void Presentation()
+    {
+        Console.WriteLine("***** Blueprint Garments Presentation *****");
+        Materials();
+        CommonWear();
+        Sales();
+        Console.WriteLine("******************************************");
+    }
+}
+
+public class TrendSetting : Clothing
+{
+    public override void Sales()
+    {
+        Console.WriteLine("Sales by Region (TrendSetting collection):");
+        Console.WriteLine("- North America");
+        Console.WriteLine("- Europe");
+        Console.WriteLine("- Asia");
+    }
+
+    public override void Materials()
+    {
+        Console.WriteLine("Materials used in TrendSetting collection:");
+        Console.WriteLine("- Wool");
+        Console.WriteLine("- Hemp");
+        Console.WriteLine("- Ramie");
+        Console.WriteLine("- Silk");
+        Console.WriteLine("- Lyocell");
+    }
+
+    public override void CommonWear()
+    {
+        Console.WriteLine("Common wear in TrendSetting collection:");
+        Console.WriteLine("- Skirts");
+        Console.WriteLine("- Trousers");
+        Console.WriteLine("- Jackets");
+        Console.WriteLine("- Blouses");
+    }
+}
+
+public class ClimateControl : Clothing
+{
+    public override void Sales()
+    {
+        Console.WriteLine("Sales by Region (ClimateControl collection):");
+        Console.WriteLine("- North America");
+        Console.WriteLine("- South America");
+        Console.WriteLine("- Australia");
+    }
+
+    public override void Materials()
+    {
+        Console.WriteLine("Materials used in ClimateControl collection:");
+        Console.WriteLine("- Cotton");
+        Console.WriteLine("- Linen");
+        Console.WriteLine("- Polyester");
+        Console.WriteLine("- Rayon");
+        Console.WriteLine("- Bamboo");
+    }
+
+    public override void CommonWear()
+    {
+        Console.WriteLine("Common wear in ClimateControl collection:");
+        Console.WriteLine("- Tops");
+        Console.WriteLine("- Dresses");
+        Console.WriteLine("- Shirts");
+    }
+}
+
+public class Sports : Clothing
+{
+    public override void Sales()
+    {
+        Console.WriteLine("Sales by Region (Sports collection):");
+        Console.WriteLine("- Europe");
+        Console.WriteLine("- Asia");
+        Console.WriteLine("- Africa");
+    }
+
+    public override void Materials()
+    {
+        Console.WriteLine("Materials used in Sports collection:");
+        Console.WriteLine("- Nylon");
+        Console.WriteLine("- Spandex");
+        Console.WriteLine("- Polyester");
+        Console.WriteLine("- Mesh");
+        Console.WriteLine("- Lycra");
+    }
+
+    public override void CommonWear()
+    {
+        Console.WriteLine("Common wear in Sports collection:");
+        Console.WriteLine("- Sportswear");
+        Console.WriteLine("- Activewear");
+        Console.WriteLine("- Athletic apparel");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Clothing trendSetting = new TrendSetting();
+        trendSetting.Presentation();
+
+        Console.WriteLine();
+
+        Clothing climateControl = new ClimateControl();
+        climateControl.Presentation();
+
+        Console.WriteLine();
+
+        Clothing sports = new Sports();
+        sports.Presentation();
+    }
+}
---
+++ assignment3/assignment3/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace CreditBureauApp
+{
+    public delegate void CreditEvent(string message);
+
+    class Customer
+    {
+        public string Name { get; set; }
+        public double Principal { get; set; }
+        public double Rate { get; set; }
+        public double Time { get; set; }
+        public double Inflation { get; set; }
+
+        public event CreditEvent Successful;
+        public event CreditEvent Unsuccessful;
+
+        public void CalculateSimpleInterest()
+        {
+            double simpleInterest = Principal * Time * Rate;
+            Successful?.Invoke($"Simple Interest for {Name}: {simpleInterest}");
+        }
+
+        public void CalculateCompoundInterest()
+        {
+            double compoundInterest = Principal * Time * Rate / 100;
+            Successful?.Invoke($"Compound Interest for {Name}: {compoundInterest}");
+        }
+
+        public void CalculateRealInterest()
+        {
+            double realInterest = Rate - Inflation;
+            Successful?.Invoke($"Real Interest for {Name}: {realInterest}");
+        }
+
+        public void PerformCreditAssessment()
+        {
+            CalculateSimpleInterest();
+            CalculateCompoundInterest();
+            CalculateRealInterest();
+        }
+
+        public void NotifyDisqualification()
+        {
+            Unsuccessful?.Invoke($"Disqualified: {Name}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an array of customers
+            Customer[][] customers = new Customer[1][]
+            {
+              new Customer[]
+                {
+                    new Customer
+                    {
+                        Name = "John",
+                        Principal = 5000,
+                        Rate = 5,
+                        Time = 2,
+                        Inflation = 1
+                    },
+                    new Customer
+                    {
+                        Name = "Sarah",
+                        Principal = 10000,
+                        Rate = 7,
+                        Time = 3,
+                        Inflation = 2
+                    }
+                }
+
+            };
+
+            // Subscribe to events for each customer
+            foreach (Customer[] customerGroup in customers)
+            {
+                foreach (Customer customer in customerGroup)
+                {
+                    customer.Successful += HandleSuccessfulCreditEvent;
+                    customer.Unsuccessful += HandleUnsuccessfulCreditEvent;
+                }
+            }
+
+            // Perform credit assessment for each customer
+            foreach (Customer[] customerGroup in customers)
+            {
+                foreach (Customer customer in customerGroup)
+                {
+                    customer.PerformCreditAssessment();
+                }
+            }
+
+            // Simulate disqualification for a customer
+            customers[0][1].NotifyDisqualification();
+
+            Console.ReadLine();
+        }
+
+        static void HandleSuccessfulCreditEvent(string message)
+        {
+            Console.WriteLine("Credit Assessment Successful:");
+            Console.WriteLine(message);
+            Console.WriteLine();
+        }
+
+        static void HandleUnsuccessfulCreditEvent(string message)
+        {
+            Console.WriteLine("Credit Assessment Unsuccessful:");
+            Console.WriteLine(message);
+            Console.WriteLine();
+        }
+    }
+}
